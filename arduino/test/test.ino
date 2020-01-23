4f40a1e7275b9f4a9d303a13b25d84351565a990
Jacob Rumpf <jrumpf@calpoly.edu>
2020-01-22 23:07:26 -0800

    Added blink to test code and created packet structure

--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -1,9 +1,13 @@
 void setup() {
-  // setup serial connection back to computer
+  // set up serial connection back to computer
   Serial.begin(115200);
+
+  // set up LED to blink
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 int msg_cnt = 0;
+bool led_state = false;
 void loop() {
   // increment message count
   msg_cnt++;
@@ -11,6 +15,15 @@ void loop() {
   // send message to computer
   Serial.print("ping ");
   Serial.println(msg_cnt);
+
+  // toggle LED
+  led_state = !led_state;
+  if(led_state) {
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
+  else {
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 
   // wait to avoid spam
   delay(1000);
